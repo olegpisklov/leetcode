ee47abfa08ac1b6cf09743b5cd38e7b64d64daf6
Oleg <margin2call@mail.ru>
2018-04-09 21:28:13 +0300

    strStr

---
+++ easy/28. Implement strStr()/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  const haystackArr = haystack.split('');
+  const needleArr = needle.split('');
+
+
+  if (!needleArr.length) {
+    return 0;
+  }
+
+  for (let i = 0; i < haystackArr.length; ++i) {
+    for (var j = 0; j < needleArr.length; ++j) {
+      if (haystackArr[i + j] !== needleArr[j]) {
+        break;
+      };
+    }
+
+    if (j === needle.length) {
+      return i;
+    }
+  }
+
+  return -1;
+};
