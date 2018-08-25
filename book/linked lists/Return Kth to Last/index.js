c670bc88dbc61453facc11d3f3588b6fb4ddf351
Oleg Pisklov <pisklovo@gmail.com>
2018-08-25 22:05:44 +0300

    Return Kth to Last

---
+++ book/linked lists/Return Kth to Last/index.js
@@ -0,0 +1,32 @@
+const LinkedList = require('../liked-list');
+
+const findK = (head, k) => {
+    const i = {value: 0};
+
+    return findKElement(head, k, i);
+};
+
+const findKElement = (node, k, i) => {
+    if (!node) {
+        return null;
+    }
+
+    const target = findKElement(node.next, k, i);
+
+    if (k === i.value) {
+        return target;
+    }
+
+    i.value += 1;
+
+    return node;
+};
+
+const list = new LinkedList(31);
+
+list.addToHead(2);
+list.addToHead(55);
+list.addToHead(77);
+list.addToHead(4);
+
+console.log(findK(list.head, 2));
