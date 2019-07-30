dac2bc878b2c62c9dcc33c3f1b5a093b6eedac64
Oleg Pisklov <pisklovo@gmail.com>
2019-07-31 00:42:20 +0300

    created a separate folder for leetcode questions

--- easy/9. Palindrome Number/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-const isPalindrome = function(x) {
-  const reversed = parseInt(x.toString().split('').reverse().join(''));
-
-  return x === reversed;
-};
