b8d72dad299700d3f41db6451ce7d15c66762842
Oleg Pisklov <pisklovo@gmail.com>
2019-08-02 00:25:04 +0300

    common ancestor

--- book/trees and graphs/First common ancestor/index.js
+++ book/trees and graphs/First common ancestor/index.js
@@ -1,14 +1,24 @@
 const findCommonAncestor = (node1, node2, root) => {
-    if (node1.value < root.value && node2.value > root.value ||
-        node1.value > root.value && node2.value < root.value) {
+    const isNode1onLeft = isOnSubtree(node1, root.left);
+    const isNode2onLeft = isOnSubtree(node2, root.right);
+
+    if (isNode1onLeft && !isNode2onLeft) {
         return root;
     }
 
-    if (node1.value < root.value && node2.value < root.value) {
-        return findCommonAncestor(node1, node2, root.left);
+    const nextSide = isNode1onLeft ? 'left' : 'right';
+
+    return findCommonAncestor(node1, node2, root[nextSide]);
+}
+
+const isOnSubtree = (node, root) => {
+    if (!root) {
+        return false;
     }
 
-    if (node1.value > root.value && node2.value > root.value) {
-        return findCommonAncestor(node1, node2, root.right);
+    if (node === root) {
+        return true;
     }
+
+    return isOnSubtree(node, root.left) || isOnSubtree(node, root.right);
 }
