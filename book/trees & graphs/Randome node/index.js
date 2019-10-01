a0d31767ea9afb79c06f93ff93e9cef661e42ce8
Oleg Pisklov <pisklovo@gmail.com>
2019-10-01 23:52:23 +0300

    Randome node

---
+++ book/trees & graphs/Randome node/index.js
@@ -0,0 +1,30 @@
+class BinaryNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.size = 0;
+    }
+
+    insert(value) {
+        if (value > this.root.value) {
+            if (this.root.right) {
+                this.insert(this.root.right.value);
+            } else {
+
+            }
+        }
+    }
+
+    find(value) {
+
+    }
+
+    getRandomeNode() {
+
+    }
+
+    delete(value) {
+
+    }
+}
