cb4209737702b5a65c254fdeb9500761eae3a64b
Oleg Pisklov <pisklovo@gmail.com>
2020-01-07 17:33:58 +0200

    Paths with sum

---
+++ book/trees & graphs/Paths with sum/index.js
@@ -0,0 +1,65 @@
+const getPathsWithSumCount = (root, sum) => {
+    let count = 0;
+
+    const traverse = (currentNode, diff) => {
+        if (diff === 0) {
+            ++count;
+        }
+
+        if (!currentNode || !currentNode.left || !currentNode.right) return;
+
+        traverse(currentNode.left, diff - currentNode.left.value);
+        traverse(currentNode.right, diff - currentNode.right.value);
+    };
+
+    const findCount = (node) => {
+        if (!node){
+            return;
+        }
+
+        traverse(node, sum - node.value);
+
+        findCount(node.left);
+        findCount(node.right);
+    }
+
+    findCount(root);
+
+    return count;
+};
+
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.rigth = null;
+    }
+}
+
+const node1 = new BinaryTreeNode(17);
+const node2 = new BinaryTreeNode(9);
+const node3 = new BinaryTreeNode(23);
+const node4 = new BinaryTreeNode(8);
+const node5 = new BinaryTreeNode(15);
+const node6 = new BinaryTreeNode(3);
+const node7 = new BinaryTreeNode(10);
+const node8 = new BinaryTreeNode(14);
+const node9 = new BinaryTreeNode(16);
+const node10 = new BinaryTreeNode(27);
+
+node1.left = node2;
+node1.right = node3;
+node2.left = node4;
+node2.right = node5;
+node4.left = node6;
+node4.right = node7;
+node5.left = node8;
+node5.right = node9;
+node3.right = node10;
+
+//                17
+//           9        23
+//        8     15          27
+//     3   10  14  16
+
+console.log(getPathsWithSumCount(node1, 40));
