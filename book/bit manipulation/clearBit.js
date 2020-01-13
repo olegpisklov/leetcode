2872116dacf92958288fb0c2f23462ae3263bb14
Oleg Pisklov <pisklovo@gmail.com>
2020-01-13 23:25:19 +0200

    clearBit

---
+++ book/bit manipulation/clearBit.js
@@ -0,0 +1,20 @@
+const clearBit = (num, i) => {
+    const mask = ~(1 << i); // 111011111;
+    return num & mask;
+};
+
+console.log(clearBit(7, 2));
+
+const clearBitsFromMostLeftToI = (num, i) => {
+    const mask = (1 << i) - 1; // 01111
+    return num & mask;
+}
+
+console.log(clearBitsFromMostLeftToI(15, 2));
+
+const clearBitsFromItoRight = (num, i) => {
+    const mask = -1 << (i + 1);
+    return num & mask;
+}
+
+console.log(clearBitsFromItoRight(15, 1));
