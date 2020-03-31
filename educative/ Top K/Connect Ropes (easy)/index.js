801ea241bb8ec0c23e0507a7ac257f7a83985514
Oleg Pisklov <pisklovo@gmail.com>
2020-03-31 12:17:39 +0300

    Connect Ropes (easy)

---
+++ educative/ Top K/Connect Ropes (easy)/index.js
@@ -0,0 +1,23 @@
+const Heap = require('./collections/heap'); //http://www.collectionsjs.com
+
+const minimum_cost_to_connect_ropes = function(ropeLengths) {
+  const minHeap = new Heap(ropeLengths, null, (a, b) => b - a);
+  let sum = 0;
+  
+  while (minHeap.length > 1) {
+    const firstMin = minHeap.pop();
+    const secondMin = minHeap.pop();
+    const currentSum = firstMin + secondMin;
+
+    sum += currentSum;
+
+    minHeap.push(currentSum);
+  }
+
+  return sum;
+};
+
+
+console.log(`Minimum cost to connect ropes: ${minimum_cost_to_connect_ropes([1, 3, 11, 5])}`)
+console.log(`Minimum cost to connect ropes: ${minimum_cost_to_connect_ropes([3, 4, 5, 6])}`);
+console.log(`Minimum cost to connect ropes: ${minimum_cost_to_connect_ropes([1, 3, 11, 5, 2])}`)
