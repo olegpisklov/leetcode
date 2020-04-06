727e5bc9ed8491c3b8ae31bdff268b88e954bce2
Oleg Pisklov <pisklovo@gmail.com>
2020-04-06 22:12:28 +0300

    string permutations recursive

--- educative/Subsets/Permutations (medium)/index.js
+++ educative/Subsets/Permutations (medium)/index.js
@@ -32,3 +32,39 @@ const result = find_permutations([1, 3, 5]);
 result.forEach((permutation) => {
     console.log(permutation);
 });
+
+
+//             ''
+//             a
+//     ba            ab
+// cba bca bac   cab acb abc
+
+const find_permutations_rec = (str) => {
+    if (!str.length) return [''];
+
+    const firstChar = str.charAt(0);
+    const leftover = str.substring(1);
+
+    const perms = find_permutations_rec(leftover);
+    const newPerms = [];
+
+    for (let i = 0; i < perms.length; ++ i) {
+        const word = perms[i];
+
+        if (!word.length) {
+            newPerms.push(firstChar);
+        } else {
+            for (let k = 0; k <= word.length; ++k) {
+                newPerms.push(word.substring(0, k) + firstChar + word.substring(k));
+            }
+        }
+    }
+
+    return newPerms;
+}
+
+console.log('Here are all the permutations 2:');
+const result1 = find_permutations_rec('abc');
+result1.forEach((permutation) => {
+    console.log(permutation);
+});
