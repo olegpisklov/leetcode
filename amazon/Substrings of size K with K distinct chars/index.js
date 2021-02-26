1d20c5a43fdda1047ade38f3ec6ad7e889d051a0
Oleg Pisklov <oleg.pisklov@namecheap.com>
2021-02-26 16:38:46 +0200

    solving questions

--- amazon/Substrings of size K with K distinct chars/index.js
+++ amazon/Substrings of size K with K distinct chars/index.js
@@ -1,31 +1,28 @@
 const main = (s, k) => {
-    const result = [];
-    const map = {};
-    const exists = new Set();
+    const charIndexMap = {};
+    const result = new Set();
     let j = 0;
 
     for (let i = 0; i < s.length; ++i) {
         const char = s[i];
 
-        if (map[char] !== undefined) {
-            j = Math.max(map[char] + 1, j);
+        if (charIndexMap[char] !== undefined) {
+            j = Math.max(charIndexMap[char] + 1, j);
         }
 
         if (i - j + 1 === k) {
             const str = s.substring(j, i + 1);
-
-            if (!exists.has(str)) {
-                result.push(str);
-                exists.add(str);
-            }
+            result.add(str);
             ++j;
         }
 
-        map[char] = i;
+        charIndexMap[char] = i;
     }
 
-    return result;
+    return Array.from(result);
 }
-// Time: O(n * k) Space: O(n)
+// Time: O(n * k), k for substring Space: O(1) or length of the alphabet if we don't count the result, else O(n)
 
  console.log(main("qwertyuiopasdfghjklzxcvbnm", 20));
+ console.log(main("xabxcd", 4));
+ console.log(main("aabcdbcd", 3));
