5d200208d879d35d21f9fc7eb03a47e62123876e
Oleg Pisklov <oleg.pisklov@namecheap.com>
2021-02-27 21:21:00 +0200

    solving questions

--- amazon/Optimizating Box Weight/index.js
+++ amazon/Optimizating Box Weight/index.js
@@ -1,4 +1,29 @@
 const main = (weights) => {
+    const freqPairs = getFrequencyPairs(weights);
+    const totalWeight = getTotalWeight(weights);
+    const maxWeight = totalWeight / 2;
+    const memo = new Array(freqPairs.length).fill(0).map(i => ({}));
+
+    const {path: resultPairs} = helper(freqPairs, 0, maxWeight, [], memo);
+
+    resultPairs.sort((a, b) => a[0] - b[0]);
+
+    const result = [];
+
+    for (let i = 0; i < resultPairs.length; ++i) {
+        const [weight, frequency] = resultPairs[i];
+
+        for (let j = 0; j < frequency; ++j) {
+            result.push(weight);
+        }
+    }
+
+    return result;
+}
+
+const getTotalWeight = (weights) => weights.reduce((acc, curWeight) => acc + curWeight, 0);
+
+const getFrequencyPairs = (weights) => {
     const freqMap = {};
 
     for (let i = 0; i < weights.length; ++i) {
@@ -8,27 +33,7 @@ const main = (weights) => {
         freqMap[weights[i]] += 1;
     }
 
-    const pairs = [];
-
-    for (let num in freqMap) {
-        pairs.push([parseInt(num), freqMap[num]]);
-    }
-
-    const maxWeight = pairs.reduce((acc, cur) => acc + cur[0] * cur[1], 0) / 2;
-    const memo = new Array(pairs.length).fill(0).map(i => ({}));
-    const {values} = helper(pairs, 0, maxWeight, [], memo);
-
-    values.sort((a, b) => a - b);
-
-    const result = [];
-
-    for (let i = 0; i < values.length; ++i) {
-        for (let j = 0; j < values[i][1]; ++j) {
-            result.push(values[i][0]);
-        }
-    }
-
-    return result;
+    return Object.entries(freqMap).map(pair => [parseInt(pair[0]), pair[1]]);
 }
 
 const helper = (pairs, index, maxWeight, path, memo) => {
@@ -36,7 +41,7 @@ const helper = (pairs, index, maxWeight, path, memo) => {
         return {
             count: path.reduce((acc, pair) => acc + pair[1], 0),
             weight: path.reduce((acc, pair) => acc + pair[0] * pair[1], 0),
-            values: [...path]
+            path: [...path]
         };
     }
 
@@ -44,7 +49,7 @@ const helper = (pairs, index, maxWeight, path, memo) => {
         return {
             count: Number.MAX_SAFE_INTEGER,
             weight: Number.MIN_SAFE_INTEGER,
-            values: []
+            path: []
         }
     }
 
@@ -71,9 +76,11 @@ const helper = (pairs, index, maxWeight, path, memo) => {
     return result;
 }
 
+// Time: (N * W) ?sorting where N is arr length, W - max weight
+
 console.time('label');
 console.log(main([5, 3, 2, 4, 1, 2]));
 console.log(main([1, 2, 5, 8, 4]));
 console.log(main([15, 20, 20, 20, 50]));
-// console.log(main([1,2,3,4,5,6,7,2,345,4,32,3,23,45,6,57,6,768,678,6,4,5,456,4,56,32,52,234,2,3,23,4,34,54,67,578,34,53,45,3,5,67,5,67,3,4,34,67,3,463456,34,563,4,56,43,456,4,56,4,36,4,67865,7,5]));
+console.log(main([1,2,3,4,5,6,7,2,345,4,32,3,23,45,6,57,6,768,678,6,4,5,456,4,56,32,52,234,2,3,23,4,34,54,67,578,34,53,45,3,5,67,5,67,3,4,34,67,3,463456,34,563,4,56,43,456,4,56,4,36,4,67865,7,5]));
 console.timeEnd('label');
