20297df9864b24c2aed20406cc0b7e5c7fb69a6f
Oleg Pisklov <oleg.pisklov@namecheap.com>
2021-02-27 10:34:27 +0200

    solving questions

--- amazon/Items in Containers/index.js
+++ amazon/Items in Containers/index.js
@@ -1,4 +1,58 @@
 const main = (str, ranges) => {
+    const n = str.length;
+    const leftPipeInd = new Array(n);
+    const rightPipeInd = new Array(n);
+    const prefixSum = new Array(n);
+
+    let curInd = -1;
+
+    for (let i = 0; i < n; ++i) {
+        if (str.charAt(i) === '|') {
+            curInd = i;
+        }
+
+        leftPipeInd[i] = curInd;
+    }
+
+    curInd = -1;
+
+    for (let i = n - 1; i >= 0; --i) {
+        if (str.charAt(i) === '|') {
+            curInd = i;
+        }
+
+        rightPipeInd[i] = curInd;
+    }
+
+    let curSum = 0;
+
+    for (let i = 0; i < n; ++i) {
+        if (str.charAt(i) === '*') {
+            curSum += 1;
+        }
+
+        prefixSum[i] = curSum;
+    }
+
+    const res = new Array(ranges.length);
+
+    for (let i = 0; i < ranges.length; ++i) {
+        const [left, right] = ranges[i];
+
+        closestLeftPipe = rightPipeInd[left];
+        closestRightPipe = leftPipeInd[right];
+
+        res[i] = prefixSum[closestRightPipe] - prefixSum[closestLeftPipe];
+    }
+
+    return res;
+}
+
+// time O(m + n) m: length of s, n: size of startIndices
+
+// https://github.com/Qiaowei2333/leetcode/blob/d437d4c7d3c4d9895b4fa7d236f9fd0c6a214f5a/algorithms/yama/11/08/ItemsInContainers.java
+
+const mainBrute = (str, ranges) => {
     let res = new Array(ranges.length);
 
     for (let i = 0; i < ranges.length; ++i) {
@@ -26,3 +80,12 @@ const main = (str, ranges) => {
 
 console.log(main('|**|*|*', [[0, 4], [1, 6]]));
 console.log(main('*|*|', [[1, 3]]));
+
+// lb = [0,0,0,3,3,5,5]
+
+// rb = [0,3,3,3,5,5,-1]
+
+// ps = [0,1,2,2,3,3,4]
+
+// leftBound = 0
+// rightBound = 3
