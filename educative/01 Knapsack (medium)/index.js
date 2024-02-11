79b2ecb84037b0a33e6eb601c0e85f61b5c86d5e
Oleg Pisklov <oleg.pisklov@namecheap.com>
2024-02-11 12:21:14 +0000

    Codestyle

--- educative/01 Knapsack (medium)/index.js
+++ educative/01 Knapsack (medium)/index.js
@@ -2,21 +2,24 @@ let solveKnapsackBU = function(profits, weights, capacity) { // Bottom Up: O(n*m
     const n = profits.length;
     // with '0' capacity we have '0' profit
     const dp = new Array(n).fill(0).map(i => new Array(capacity + 1).fill(0));
-  
+
     // if we have only one weight, we will take it if it is not more than the capacity
     for (let c = 0; c <= capacity; c++) {
         if (weights[0] <= c) {
             dp[0][c] = profits[0];
         }
     }
-  
+
     for (let i = 1; i < n; ++i) {
+        const currentWeight = weights[i];
+        const currentProfit = profits[i];
+
         for (let c = 1; c <= capacity; ++c) {
-            const profit1 = dp[i - 1][c]; // do not take cur weight
+            const profit1 = dp[i - 1][c]; // profit without taking current weight
             let profit2 = 0;
     
-            if (weights[i] <= c) { // take cur weight
-                profit2 = Math.max(dp[i - 1][c], dp[i - 1][c - weights[i]] + profits[i]);
+            if (currentWeight <= c) { // take cur weight
+                profit2 = Math.max(profit1, dp[i - 1][c - currentWeight] + currentProfit);
             }
     
             dp[i][c] = Math.max(profit1, profit2);
@@ -56,5 +59,5 @@ const helper = (profits, weights, capacity, i, memo) => {
 
 var profits = [1, 6, 10, 16];
 var weights = [1, 2, 3, 5];
-console.log(`Total knapsack profit: ---> ${solveKnapsack(profits, weights, 7)}`);
+console.log(`Total knapsack profit: ---> ${solveKnapsackBU(profits, weights, 7)}`);
 console.log(`Total knapsack profit: ---> ${solveKnapsack(profits, weights, 6)}`);
