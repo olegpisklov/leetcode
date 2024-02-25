a3575e6fbb5982d22e93270c0697329402ea8780
Oleg Pisklov <oleg.pisklov@namecheap.com>
2024-02-25 14:43:44 +0000

    Fibonacci DP

--- book/fiboncci.js
+++ book/fiboncci.js
@@ -1,3 +1,5 @@
+const pos = 40
+
 function fib(n) {
     if (n < 2) {
         return n;
@@ -6,4 +8,25 @@ function fib(n) {
     return fib(n - 2) + fib(n - 1);
 }
 
-console.log(fib(7));
+console.time('fib')
+console.log(fib(pos));
+console.timeEnd('fib')
+
+const dp = new Array(pos + 1).fill(-1);
+
+function fibDP(n) {
+    if (n < 2) {
+        return n;
+    }
+    if (dp[n] !== -1) {
+        return dp[n];
+    }
+
+    dp[n] = fibDP(n - 2) + fibDP(n - 1); 
+
+    return dp[n];
+}
+
+console.time('fibDP')
+console.log(fibDP(pos));
+console.timeEnd('fibDP')
